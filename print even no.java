7a87650f4297a95d55fff632ec23bd1a15ee0493
susmidha <36623596+susmidha@users.noreply.github.com>
2018-05-19 20:26:16 -0700

    Create print even no.java

---
+++ print even no.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	
+		Scanner scan=new Scanner (System.in);
+		int x=scan.nextInt();
+		
+		for(int i=1;i<=x;i++)
+		{
+			if(i%2==0)
+			{
+				System .out.print(i);	
+			}
+			
+		}
+	}
+}
