5451c65f8bf72094b863e869f23cf8597e4bd15b
susmidha <36623596+susmidha@users.noreply.github.com>
2018-05-21 07:11:38 -0700

    Create arithematic.java

---
+++ arithematic.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	
+		Scanner scan =new Scanner(System.in);
+		int n=scan.nextInt();
+		int a=scan.nextInt();
+		int d=scan.nextInt();
+	          int sum=a+(n-1)*d;
+	          System.out.println(sum);
+	          
+	}
+}
