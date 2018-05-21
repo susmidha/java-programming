ffd82cb9cae73735262d40d6aa0a57d63a16d90a
susmidha <36623596+susmidha@users.noreply.github.com>
2018-05-21 06:54:07 -0700

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,20 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	
+		Scanner scan =new Scanner(System.in);
+		int f=scan.nextInt();
+		int num1=scan.nextInt();
+		int num2=scan.nextInt();
+		for(int i=num1;i<=num2;i++)
+		{
+		 
+		 System.out.print(f+"*"+i+"="+(f*i)+"\n");
+                          
+			
+		}
+		
+	}
+}
