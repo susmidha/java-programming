96d3f045d7b027fd70b70e26570f70813b25222d
susmidha <36623596+susmidha@users.noreply.github.com>
2018-05-30 04:59:29 -0700

    Create Append_Dot.java

---
+++ Append_Dot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan=new Scanner(System.in);
+	String a=scan.nextLine();
+	String s=a+".";
+	System.out.println(s);
+		
+	}
+}
