5f906ad5f754f73d250ae0d3b1adc5991cfb1599
susmidha <36623596+susmidha@users.noreply.github.com>
2018-06-01 05:27:33 -0700

    Create Countofchar.java

---
+++ Countofchar.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class GFG
+{
+	// Method that return count of the given
+	// character in the string
+	public static int count(String s, char c)
+	{
+		int res = 0;
+
+		for (int i=0; i<s.length(); i++)
+		{
+			// checking character in string
+			if (s.charAt(i) == c)
+			res++;
+		} 
+		return res;
+	}
+	
+	// Driver method
+	public static void main(String args[])
+	{
+		String str= "Hello world";
+		char c = 'l';
+		System.out.println(count(str, c));
+	}
+}
