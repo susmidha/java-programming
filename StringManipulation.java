11babc1a66b9775bb55484c28f5be93775c5c056
susmidha <36623596+susmidha@users.noreply.github.com>
2018-06-04 20:23:14 -0700

    Create StringManipulation.java

---
+++ StringManipulation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s0="";
+		String s1="HELLO";
+		String s2=new String("how are you");
+		System.out.println(s1.length());
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.concat(s2));
+		System.out.println(s0.isEmpty());
+		System.out.println(s1.toLowerCase());
+		System.out.println(s2.toUpperCase());
+		System.out.println(s1.replace("HELLO","Sarava"));
+		System.out.println(s2.contains("are"));
+	            System.out.println(s2.endsWith("you"));
+		
+		
+		
+	}
+}
