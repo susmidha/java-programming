06500c66a4d9ddda5ec6ce3aa132e4001c844001
susmidha <36623596+susmidha@users.noreply.github.com>
2018-06-27 14:16:04 +0530

    Update PRIME_Index.java

--- PRIME_Index.java
+++ PRIME_Index.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
 
 
-public class Prime_Num_Index {
+public class Prime_Index {
 
 	/**
 	 * @param args
 	 */
-	
-	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    Scanner s=new Scanner(System.in);
-    int n=s.nextInt();
-    int count,c=0;
-    for(int i=2;i<n;i++){
-    	count=0;
-    	for(int j=2;j<i;j++){
-    		if(i%j==0){
-    			count++;
-    			break;
-    		}
-    	}
-    	
-    	if(count==0){	
-    	       c++;
-    	          
-    	
-    	}
-    	
-    }
-    
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int count;
+		int s1=0;
+		for(int i=2;i<100;i++){
+             count=0;
+			for(int j=2;j<i;j++){
+				if(i%j==0){
+					count++;
+	                break;
+					
+				}
+					
+			}
+			if(count==0){
+				s1++;
+				if(s1==n)
+				System.out.println(i);
+				
+			}
+			
+			
+		}
+	
+		
+
 	}
 
 }
