ab713c38ce0ed295160199522125e71e8bfb7ece
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-08 17:03:54 -0700

    Create Pos_neg.java

---
+++ Pos_neg.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=-5,rem=0;
+		rem=a%10;
+		System.out.println(Math.abs(rem));
+	}
+}
