2fe0a4351134b104054d0b3a80e7a6e1b6c8555b
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-12 06:11:19 -0700

    Update Count_char.java

--- Count_char.java
+++ Count_char.java
@@ -23,28 +23,3 @@ System.out.println(count);
 	}
 
 }
-import java .util.*;
-public class Count_char {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner scan=new Scanner(System.in);
-String s=scan.nextLine();
-int count=0;
-String t[]=new String[s.length()];
-
-for(int i=0;i<s.length();i++) {
-	if(Character.isWhitespace(s.charAt(i))) {
-		count--;
-		//System.out.println(count);
-	}
-	else {
-		count++;
-		//System.out.println(count);
-	}
-}
-System.out.println(count);
-
-	}
-
-}
