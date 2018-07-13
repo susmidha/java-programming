0f926274628ccf3a3aa724ca0018482e08169c17
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-13 06:26:44 -0700

    Create Swap_num.java

---
+++ Swap_num.java
@@ -0,0 +1,17 @@
+import  java.util.*;
+public class Swap_num {
+  
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int t=a;
+		a=b;
+		b=t;
+                 System.out.println(a+" "+b);
+	}
+
+}
