e0750adc306a0effc392904727c40c43497e4cdf
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-14 06:32:05 -0700

    Create Pattern_pramid.java

---
+++ Pattern_pramid.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		for(int i=7;i>1;i--){
+			for(int j=1;j<=i;j++){
+			System.out.print(j+" ");
+		}
+		System.out.println(" ");
+	}
+		for(int i=1;i<=7;i++){
+			for(int j=1;j<=i;j++){
+			System.out.print(j+" ");
+		}
+		System.out.println(" ");
+	}
+	}
+}
