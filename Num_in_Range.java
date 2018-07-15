207f4c8aa637a1fa63a31154e042d175c54288fd
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-14 20:24:32 -0700

    Create Num_in_Range.java

---
+++ Num_in_Range.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+	            int n=s.nextInt();
+	            int count=0;
+	            for(int i=1;i<=10;i++){
+	            	if(i==n){
+	            		count=1;
+	            	
+	            	}
+	            	
+	            }
+	            if(count==1){
+	            	System.out.println("yes");
+	            }
+	            else{
+	            	System.out.println("no");
+	            	
+	            }
+	            
+	            
+	           
+		
+	}
+}
