cc48fc34497d55d0ba9395a7307607b841bc799d
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-14 20:15:07 -0700

    Create String_concat_set7.java

---
+++ String_concat_set7.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+		String a[]=str1.split(" ");
+		System.out.println(a[0]+a[1]);
+	           
+	           
+		
+	}
+}
