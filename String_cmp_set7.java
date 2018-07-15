5c84cf42e38f994a9ef13fc7ef7c1863f90f9ecc
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-14 20:06:28 -0700

    Create String_cmp_set7.java

---
+++ String_cmp_set7.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+	            String str2=s.nextLine();
+	            int n1=str1.length();
+	            int n2=str2.length();
+	            if(n1>n2){
+	            	System.out.println(str1);
+	        
+	            }
+	            else if(n1<n2){
+	            	System.out.println(str2);
+	        	
+	            }
+	            else{
+	            		System.out.println(str1);
+	            }
+		
+		
+	}
+}
